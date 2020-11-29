57dca5e2a8d0e9583cbd06a3503ce6e064ede9e4
Federico <polli.f@outlook.it>
2020-11-29 17:31:43 +0100

    Refactored code

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,9 +1,10 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int score_player1 = 0;
     private String player1Name;
+
+    private int score_player2 = 0;
     private String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
@@ -12,65 +13,89 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (player1Name.equals(playerName))
+            score_player1 += 1;
         else
-            m_score2 += 1;
+            score_player2 += 1;
     }
 
-    public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+    public String getScore(){
+        if (players_tied()) {
+            return equal_score();
+        } else if (advantage()) {
+            return "Advantage "+player_with_higher_score();
+        } else if (game_won()) {
+            return "Win for " + player_with_higher_score();
+        } else {
+            return other_scores();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
+
+    private boolean advantage() {
+        return both_players_have_3_or_more_points() && player_has_1_more_point();
+    }
+
+    private boolean player_has_1_more_point() {
+        return Math.abs(score_player1 - score_player2) == 1;
+    }
+
+    private boolean both_players_have_3_or_more_points() {
+        return score_player1 >= 3 && score_player2 >= 3;
+    }
+
+    private String other_scores() {
+        return correct_term(score_player1)+"-"+correct_term(score_player2);
+    }
+
+    private boolean game_won() {
+        return player_scored_more_than_4_points() && player_has_two_points_more();
+    }
+
+    private boolean player_has_two_points_more() {
+        return Math.abs(score_player1 - score_player2) >= 2;
+    }
+
+    private boolean player_scored_more_than_4_points() {
+        return score_player2 >= 4 || score_player1 >= 4;
+    }
+
+    private String player_with_higher_score() {
+        if (score_player1 >= score_player2)
+            return player1Name;
+        else
+            return player2Name;
+    }
+
+    private String equal_score() {
+        if (score_player1 < 3) {
+            return correct_term(score_player1)+"-All";
+        } else  {
+            return "Deuce";
+        }
+            
+    }
+
+    private String correct_term(int score) {
+        String term="";
+        switch (score) {
+            case 0:
+                term+="Love";
+                break;
+            case 1:
+                term+="Fifteen";
+                break;
+            case 2:
+                term+="Thirty";
+                break;
+            case 3:
+                term+="Forty";
+                break;
+        }
+        return term;
+    }
+
+    private boolean players_tied() {
+        return score_player1 == score_player2;
+    }
+
 }
